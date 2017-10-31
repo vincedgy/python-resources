206c411e1474bbda161d760ae03944e1a1341866
Vincent <vincent.dagoury@gmail.com>
2017-10-31 21:39:38 +0100

    Updating stage3

--- closures/closure.py
+++ closures/closure.py
@@ -3,15 +3,17 @@
 from __future__ import print_function
 
 def decorator_function(original_function):
-    def wrapper_function():
+    def wrapper_function(*args, **kwargs):
         print('wrapper executer this before {}'.format(original_function.__name__))
-        return original_function()
+        return original_function(*args, **kwargs)
     return wrapper_function
 
 @decorator_function
 def display():
     print('display function ran')
 
-#display = decorator_function(display)
+@decorator_function
+def display_info(name, age):
+    print('display_info ran with arguments ({}, {})'.format(name, age))
 
-display()
+display_info('John', 25)
