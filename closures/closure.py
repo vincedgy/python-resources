c5c9cbdddf02cd89ef28efdbccb898b29daf0853
Vincent <vincent.dagoury@gmail.com>
2017-10-31 21:49:34 +0100

    Updating stage5

--- closures/closure.py
+++ closures/closure.py
@@ -2,19 +2,17 @@
 """ """
 from __future__ import print_function
 
-class decorator_class(object):
-    def __init__(self, original_function):
-        self.original_function = original_function
-    def __call__(self, *args, **kwargs):
-        print('- call executed this before {}'.format(self.original_function.__name__))
-        return self.original_function(*args, **kwargs)
- 
+def decorator_function(original_function):
+    def wrapper_function(*args, **kwargs):
+        print('- wrapper executed this before {}'.format(original_function.__name__))
+        return original_function(*args, **kwargs)
+    return wrapper_function
 
-@decorator_class
+@decorator_function
 def display():
     print('display function ran')
 
-@decorator_class
+@decorator_function
 def display_info(name, age):
     print('display_info ran with arguments ({}, {})'.format(name, age))
 
