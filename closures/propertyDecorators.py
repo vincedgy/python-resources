2975eb0113e26506f74fb24c2c0bc6a74550877e
Vincent <vincent.dagoury@gmail.com>
2017-10-31 23:04:30 +0100

    Updating stage6 with property decorators

--- closures/propertyDecorators.py
+++ closures/propertyDecorators.py
@@ -1,4 +1,4 @@
-# Decorators 
+# Property Decorators 
 """ """
 from __future__ import print_function
 from functools import wraps
@@ -9,14 +9,20 @@ class Employee:
     def __init__(self, first, last):
         self.first = first
         self.last = last
-        self.email = self.first + '.' + self.last + '@email.com'
     
-    def fullname(self)
+    @property
+    def email(self):
+        return '{}.{}@email.com'.format(self.first, self.last)
+
+    @property
+    def fullname(self):
         return '{} {}'.format(self.first, self.last)
 
    
 emp_1 = Employee('John', 'Smith')
 
+emp_1.first = 'Jim'
+
 print(emp_1.first)
-print(emp_1.last)
-print(emp_1.fullname())
+print(emp_1.email)
+print(emp_1.fullname)
