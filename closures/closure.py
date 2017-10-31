5a60f3f6798cea0c4d0c86eb40cbde56e96f6f45
Vincent <vincent.dagoury@gmail.com>
2017-10-31 22:49:35 +0100

    Updating stage5

--- closures/closure.py
+++ closures/closure.py
@@ -1,28 +1,37 @@
 # Decorators 
 """ """
 from __future__ import print_function
+from functools import wraps
+import time
 
 def my_logger(orig_func):
     import logging
     logging.basicConfig(filename='{}.log'.format(orig_func.__name__), level=logging.INFO)
 
+    @wraps(orig_func)
     def wrapper(*args, **kwargs):
         logging.info('Ran with args: {} and kwargs: {}'.format(args, kwargs))
         return orig_func(*args, **kwargs)
-    
+   
     return wrapper
 
 def my_timer(orig_func):
     import time
+    
+    @wraps(orig_func)
     def wrapper(*args, **kwargs):
         t1 = time.time()
         result = orig_func(*args, **kwargs)
         t2 = time.time() - t1
         print('{} ran into {}'.format(orig_func.__name__, t2))
+        return result
+
     return wrapper
 
 @my_logger
+@my_timer
 def display_info(name, age):
+    time.sleep(1)
     print('display_info ran with arguments ({}, {})'.format(name, age))
 
-display_info('John', 25)
+display_info('Hank', 27)
