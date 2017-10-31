8f1dec6053623cd9aa4abba40e401957078b2cae
Vincent <vincent.dagoury@gmail.com>
2017-10-31 21:13:36 +0100

    Updating stage2.1

--- closures/closure.py
+++ closures/closure.py
@@ -8,9 +8,8 @@ def decorator_function(original_function):
         return original_function()
     return wrapper_function
 
+@decorator_function
 def display():
     print('display function ran')
 
-decorator_display = decorator_function(display)
-
-decorator_display()
+display()
