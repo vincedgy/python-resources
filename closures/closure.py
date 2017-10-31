6abfe99ea64c3a45bb3c0fea16c3719bccb93f79
Vincent <vincent.dagoury@gmail.com>
2017-10-31 21:12:41 +0100

    Updating stage2.1

--- closures/closure.py
+++ closures/closure.py
@@ -4,6 +4,7 @@ from __future__ import print_function
 
 def decorator_function(original_function):
     def wrapper_function():
+        print('wrapper executer this before {}'.format(original_function.__name__))
         return original_function()
     return wrapper_function
 
