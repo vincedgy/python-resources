6e056d274475c4bdfc3563e66510686d5b5ea28f
Vincent <vincent.dagoury@gmail.com>
2017-10-31 22:59:15 +0100

    Updating stage6 with property decorators

--- closures/propertyDecorators.py
+++ closures/propertyDecorators.py
@@ -4,34 +4,19 @@ from __future__ import print_function
 from functools import wraps
 import time
 
-def my_logger(orig_func):
-    import logging
-    logging.basicConfig(filename='{}.log'.format(orig_func.__name__), level=logging.INFO)
-
-    @wraps(orig_func)
-    def wrapper(*args, **kwargs):
-        logging.info('Ran with args: {} and kwargs: {}'.format(args, kwargs))
-        return orig_func(*args, **kwargs)
-   
-    return wrapper
-
-def my_timer(orig_func):
-    import time
+class Employee:
     
-    @wraps(orig_func)
-    def wrapper(*args, **kwargs):
-        t1 = time.time()
-        result = orig_func(*args, **kwargs)
-        t2 = time.time() - t1
-        print('{} ran into {}'.format(orig_func.__name__, t2))
-        return result
+    def __init__(self, first, last):
+        self.first = first
+        self.last = last
+        self.email = self.first + '.' + self.last + '@email.com'
+    
+    def fullname(self)
+        return '{} {}'.format(self.first, self.last)
 
-    return wrapper
+   
+emp_1 = Employee('John', 'Smith')
 
-@my_logger
-@my_timer
-def display_info(name, age):
-    time.sleep(1)
-    print('display_info ran with arguments ({}, {})'.format(name, age))
-
-display_info('Tom', 46)
+print(emp_1.first)
+print(emp_1.last)
+print(emp_1.fullname())
