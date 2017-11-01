08c6ead03c19695fae886ce6d00e219bc3787144
Vincent <vincent.dagoury@gmail.com>
2017-11-01 16:21:23 +0100

    Adding stage7 with metaclass

---
+++ closures/metaclasses/README.md
@@ -0,0 +1,3 @@
+# Metaclass
+
+Get information on the mother class that a child class don't implement a method the mother eagerly wants its child to implement.
---
+++ closures/metaclasses/user.py
@@ -0,0 +1,8 @@
+# Decorators 
+""" """
+from __future__ import print_function
+from library import Base
+
+class Derived(Base):
+    def bar(self):
+        return 'bar'
