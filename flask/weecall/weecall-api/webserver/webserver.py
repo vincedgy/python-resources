0f4c88ba4441e528dc8808dfac18ad4a9fd9d779
Vincent <vincent.dagoury@gmail.com>
2020-04-26 21:48:49 +0200

    Adding templates to webserver

--- flask/weecall/weecall-api/webserver/webserver.py
+++ flask/weecall/weecall-api/webserver/webserver.py
@@ -1,4 +1,5 @@
 import os
+import datetime
 from flask import Flask, redirect, render_template
 import colorama
 from flask_cors import CORS
@@ -21,19 +22,29 @@ posts = [
     }
 ]
 
-@app.route('/home')
-def get_home():
-    return render_template('welcome.html', posts=posts)
+current_datetime = datetime.datetime.now()
+
+def render(template, **kwargs):
+    kwargs['current_year']=current_datetime.year
+    return render_template(template, **kwargs)
+
+@app.route('/welcome')
+def get_welcome():
+    return render('welcome.html')
+
+@app.route('/posts')
+def get_posts():
+    return render('posts.html', posts=posts, title="Posts")
 
 
 @app.route('/about')
 def get_about():
-    return render_template('about.html')
+    return render('about.html', title="About")
 
 
 @app.route('/')
 def default():
-    return redirect("/home", code=302)
+    return redirect("/welcome", code=302)
 
 if __name__ == '__main__':
     # Bind to PORT if defined, otherwise default to 5000.
